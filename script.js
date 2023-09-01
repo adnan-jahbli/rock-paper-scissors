4cfe6704973d4089a16e29042d26f96cbc086d66
adnan-jahbli <adnan.jahbli@gmail.com>
2023-09-01 19:09:28 +0100

    adding a pop up after the game ends

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function playRound(playerSelection, computerSelection) {
             return "A wonderful tie";
         }
         else if(computerSelection == "Paper") {
-            return "You Lose! Paper beats Rock.";
+            return "You lose! Paper beats Rock.";
         }
         else {
             return "You wun! Rock beats Scissors.";
@@ -35,7 +35,7 @@ function playRound(playerSelection, computerSelection) {
             return "A wonderful tie";
         }
         else if(computerSelection == "Scissors") {
-            return "You Lose! Scissors beats Paper.";
+            return "You lose! Scissors beats Paper.";
         }
         else {
             return "You wun! Paper beats Rock.";
@@ -46,7 +46,7 @@ function playRound(playerSelection, computerSelection) {
             return "A wonderful tie";
         }
         else if(computerSelection == "Rock") {
-            return "You Lose! Rock beats Scissors.";
+            return "You lose! Rock beats Scissors.";
         }
         else {
             return "You wun! Scissors beats Paper.";
@@ -60,6 +60,10 @@ let computerScore = document.querySelector('.computer_score');
 let userScore = document.querySelector('.user_score');
 let msg = document.createElement('h3');
 let scoreDiv = document.querySelector('.score');
+let cScore = 0, uScore = 0;
+userScore.textContent += uScore.toString();
+computerScore.textContent += cScore.toString();
+const TRIES = 5;
 
 buttons.forEach((button) => {
     button.addEventListener('click', function() {
@@ -78,5 +82,53 @@ buttons.forEach((button) => {
         userScore.setAttribute("style", `background-image: url(./images/${userChoice}.png); background-size: 90px;`);
         msg.textContent = playRound(userChoice, computerChoice);
         scoreDiv.appendChild(msg);
+
+        let text;
+        if (msg.textContent.startsWith("You wun")) {
+            uScore += 1;
+            text = userScore.textContent;
+            userScore.textContent = text.slice(0, -1) + uScore;
+        }
+        else if (msg.textContent.startsWith("You lose")) {
+            cScore += 1;
+            text = computerScore.textContent;
+            computerScore.textContent = text.slice(0, -1) + cScore;
+        }
+
+        if (uScore === TRIES || cScore === TRIES) {
+            let container = document.querySelector('.container');
+            let popUpContainer = document.createElement('div');
+            let popUp = document.createElement('div');
+            let popUpHeader = document.createElement('p');
+            let popUpMsg = document.createElement('p');
+            let popUpButton = document.createElement('button');
+
+            popUpContainer.classList = "popUpContainer";
+            popUp.classList = "popUp";
+            popUpHeader.classList = "popUpHeader";
+            popUpMsg.classList = "popUpMsg";
+            popUpButton.classList = "popUpButton";
+            popUpHeader.textContent = 'Game Over!';
+            if (uScore > cScore) {
+                popUpMsg.textContent = 'You wun! congrats :)';
+            }
+            else {
+                popUpMsg.textContent = 'You lost, unfortunately :(';
+            }
+            popUpButton.textContent = 'Play again';
+            container.appendChild(popUpContainer);
+            container.appendChild(popUp);
+            popUp.appendChild(popUpHeader);
+            popUp.appendChild(popUpMsg);
+            popUp.appendChild(popUpButton);
+
+            popUpButton.addEventListener('click', function() {
+                select_sound.currentTime = 0;
+                select_sound.play();
+
+                // reloading the page to start the game over
+                location.reload();
+            })
+        }
     })
 });
